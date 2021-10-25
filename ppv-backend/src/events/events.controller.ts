5c5fe5e26fd5d7017314e38538adec198bed1d51
alexandra-doris <62300754+alexandra-doris@users.noreply.github.com>
2021-10-25 22:48:06 +0300

    Add Get, Post events

--- ppv-backend/src/events/events.controller.ts
+++ ppv-backend/src/events/events.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { EventsService } from './events.service';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+    constructor(private eventService: EventsService) {
+
+    }
+
+    @Get()
+    async all() {
+        return this.eventService.find();
+    }
+
+    @Post()
+    async create(
+        @Body("title") title: string,
+        @Body("posterUrl") posterUrl: string,
+        @Body("description") description: string,
+        @Body("price") price: string,
+        @Body("beginDate") beginDate: Date,
+        @Body("endDate") endDate: string,
+        @Body("type") type: string,
+        @Body("source") source: string,
+        @Body("videoUrl") videUrl: string,
+    ) {
+        return this.eventService.create({
+            title, posterUrl, description,
+            price, beginDate, endDate,
+            type, source, videUrl
+        })
+    }
+}
--- ppv-backend/src/events/events.service.ts
+++ ppv-backend/src/events/events.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Event} from "./events.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+
+    constructor(
+        @InjectRepository(Event) private readonly eventRepository: Repository<Event>
+    ) {
+
+    }
+    
+    async find(): Promise<Event[]> {
+        return this.eventRepository.find();
+    }
+}
