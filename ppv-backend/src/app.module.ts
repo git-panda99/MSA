bfc999be9a76dae28e3cb3b133b172b4a11c306f
alexandra-doris <62300754+alexandra-doris@users.noreply.github.com>
2021-11-02 15:07:19 +0200

    Add users

--- ppv-backend/src/app.module.ts
+++ ppv-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsModule } from './events/events.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AppService } from './app.service';
       synchronize: true,
     }),
     EventsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
