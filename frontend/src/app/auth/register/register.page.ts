869665285faee832bbbe010b561ed9519106f038
paula-christine <paula.ciorba@gmail.com>
2021-11-16 15:49:52 +0200

    Design Changes

--- frontend/src/app/auth/register/register.page.ts
+++ frontend/src/app/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
   }
+
+ ngOnInit() {
+ }
+
+ onFormSubmit() {
+   this.router.navigate(['/login']);
+ }
 
 }
