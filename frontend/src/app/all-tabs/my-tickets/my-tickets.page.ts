6d3978797c3b197d35c774f12c9cf61ea7875425
paula-christine <paula.ciorba@gmail.com>
2022-01-09 00:20:07 +0200

    Add user ticket list

--- frontend/src/app/all-tabs/my-tickets/my-tickets.page.ts
+++ frontend/src/app/all-tabs/my-tickets/my-tickets.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth/auth.service';
+import { EventVideo } from 'src/app/shared/event-video';
+import { Ticket } from 'src/app/shared/ticket';
+import { TicketService } from 'src/app/shared/ticket.service';
 
 @Component({
   selector: 'app-mytickets',
@@ -6,7 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['my-tickets.page.scss']
 })
 export class MyTicketsPage {
+  userId;
+  userTickets: any = [];
 
-  constructor() {}
+  constructor(
+    private ticketService: TicketService,
+    private authService: AuthService
+  ) {
+
+  }
+
+  ngOnInit()
+  { 
+    
+  }
+
+  ionViewWillEnter() {
+    this.authService.getProfileId().then((res) => {
+      this.userId = res;
+      this.ticketService.getTicketListByUserId(this.userId).subscribe((res) => {
+      console.log("got event list")
+      console.log(res)
+      this.userTickets = res;
+    })
+    });
+    
+  }
 
 }
