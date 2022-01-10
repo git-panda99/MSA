297689e07de65e7e4eaa64ef4118859531627f85
alexandra-doris <62300754+alexandra-doris@users.noreply.github.com>
2022-01-10 17:06:20 +0200

    Delete comment

--- ppv-backend/src/mail/mail.module.ts
+++ ppv-backend/src/mail/mail.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -33,28 +32,6 @@ import { join } from 'path';
       }),
       inject: [ConfigService],
     }),
-      // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
-      // or
-      /*transport: {
-        host: 'mail.bpstream.ro',
-        port: 465,
-        secure: true,
-        auth: {
-          user: 'noreply@bpstream.ro',
-          pass: ',=hp9vQ4FWj7',
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@bpstream.ro>',
-      },
-      template: {
-        dir:  __dirname + "/templates",
-        adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
-        options: {
-          strict: true,
-        },
-      },
-    }),*/
   ],
   providers: [MailService],
   exports: [MailService]
